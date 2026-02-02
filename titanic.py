33f00422efd84daa34d98f7f6abddea29dd8d73b
boubaker <boubaker.asaadi@ensae.fr>
2026-02-02 09:44:59 +0000

    script fonctionnel

--- titanic.py
+++ titanic.py
@@ -1,9 +1,8 @@
 import os
 import argparse
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
+from dotenv import load_dotenv
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -12,8 +11,51 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import confusion_matrix
-from dotenv import load_dotenv
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+def create_pipeline(n_trees, numeric_features=["Age", "Fare"],\
+    categorical_features=["Embarked", "Sex"], max_depth=None, max_features='sqrt'):
+
+    ## Encoder les données imputées ou transformées.
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", MinMaxScaler()),
+        ]
+    )
+
+    categorical_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("onehot", OneHotEncoder()),
+        ]
+    )
+
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("Preprocessing numerical", numeric_transformer, numeric_features),
+            (
+                "Preprocessing categorical",
+                categorical_transformer,
+                categorical_features,
+            ),
+        ]
+    )
+
+    pipe = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("classifier", RandomForestClassifier(n_estimators=n_trees, max_features=max_features,
+                                                max_depth=max_depth)),
+        ]
+    )
+    return pipe
+
+def evaluate(y_test, y_pred):
+    rdmf_score = accuracy_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+    return rdmf_score, cm
 
 # Load environment variables
 load_dotenv()
@@ -28,85 +70,14 @@ print(f"n_trees: {n_trees}")
 
 
 jeton_api = os.environ.get("JETON_API", "")
-print("jeton_api")
 if jeton_api.startswith("$"):
     print("API token has been configured properly")
 else:
     print("API token has not been configured")
 
-# os.chdir('/home/coder/work/ensae-reproductibilite-application')
-TrainingData = pd.read_csv("data.csv")
+BASE_PATH = os.environ.get("BASE_PATH", "")
 
-TrainingData.head()
-
-
-TrainingData["Ticket"].str.split("/").str.len()
-
-TrainingData["Name"].str.split(",").str.len()
-
-TrainingData.isnull().sum()
-
-
-## Un peu d'exploration et de feature engineering
-
-### Statut socioéconomique
-
-fig, axes = plt.subplots(
-    1, 2, figsize=(12, 6)
-)  # layout matplotlib 1 ligne 2 colonnes taile 16*8
-fig1_pclass = sns.countplot(data=TrainingData, x="Pclass", ax=axes[0]).set_title(
-    "fréquence des Pclass"
-)
-fig2_pclass = sns.barplot(
-    data=TrainingData, x="Pclass", y="Survived", ax=axes[1]
-).set_title("survie des Pclass")
-
-
-### Age
-
-sns.histplot(data=TrainingData, x="Age", bins=15, kde=False).set_title(
-    "Distribution de l'âge"
-)
-plt.show()
-
-## Encoder les données imputées ou transformées.
-
-numeric_features = ["Age", "Fare"]
-categorical_features = ["Embarked", "Sex"]
-
-numeric_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="median")),
-        ("scaler", MinMaxScaler()),
-    ]
-)
-
-categorical_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="most_frequent")),
-        ("onehot", OneHotEncoder()),
-    ]
-)
-
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("Preprocessing numerical", numeric_transformer, numeric_features),
-        (
-            "Preprocessing categorical",
-            categorical_transformer,
-            categorical_features,
-        ),
-    ]
-)
-
-pipe = Pipeline(
-    [
-        ("preprocessor", preprocessor),
-        ("classifier", RandomForestClassifier(n_estimators=n_trees)),
-    ]
-)
-
+TrainingData = pd.read_csv(os.path.join(BASE_PATH, "data.csv"))
 
 # splitting samples
 y = TrainingData["Survived"]
@@ -119,26 +90,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 pd.concat([X_train, y_train], axis = 1).to_csv("train.csv")
 pd.concat([X_test, y_test], axis = 1).to_csv("test.csv")
 
-jetonapi = "$trotskitueleski1917"
-
-
 # Random Forest
-
-
 # Ici demandons d'avoir n_trees arbres
+pipe = create_pipeline(n_trees)
 pipe.fit(X_train, y_train)
-
 
 # calculons le score sur le dataset d'apprentissage et sur le dataset
 # de test (10% du dataset d'apprentissage mis de côté)
 # le score étant le nombre de bonne prédiction
-rdmf_score = pipe.score(X_test, y_test)
-rdmf_score_tr = pipe.score(X_train, y_train)
+
+y_pred = pipe.predict(X_test)
+rdmf_score, cm = evaluate(y_test, y_pred)
+
 print(f"{rdmf_score:.1%} de bonnes réponses sur les données de test pour validation")
 print(20 * "-")
 print("matrice de confusion")
-print(confusion_matrix(y_test, pipe.predict(X_test)))
+print(cm)
 
-MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
+
 
