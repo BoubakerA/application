35801f2c5a0ff251003f2a374912b5e54517ff05
boubaker <boubaker.asaadi@ensae.fr>
2026-03-01 14:00:20 +0000

    load production model from mlflow

--- app/api.py
+++ app/api.py
@@ -1,18 +1,42 @@
 """A simple API to expose our trained RandomForest model for Tutanic survival."""
 
+import logging
 from fastapi import FastAPI
-from joblib import load
 import pandas as pd
+import mlflow
 
 
-model = load("model.joblib")
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.DEBUG,
+    handlers=[logging.FileHandler("api.log"), logging.StreamHandler()],
+)
+
+# Preload model -------------------
+
+logging.info(
+    "Getting model from MLFlow"
+)
+
+model_name = "production"
+model_version = "latest"
+
+# Load the model from the Model Registry
+model_uri = f"models:/{model_name}/{model_version}"
+model = mlflow.sklearn.load_model(model_uri)
+
+# Define app -------------------------
+
 
 app = FastAPI(
-    title="Démonstration du modèle de prédiction de survie sur le Titanic",
+    title="Prédiction de survie sur le Titanic",
     description=
-    "<b>Application de prédiction de survie sur le Titanic</b> 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
+    "Application de prédiction de survie sur le Titanic 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
         "<br><br><img src=\"https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg\" width=\"200\">"
     )
+
 
 @app.get("/", tags=["Welcome"])
 def show_welcome_page():
@@ -22,16 +46,20 @@ def show_welcome_page():
 
     return {
         "Message": "API de prédiction de survie sur le Titanic",
-        "Model_name": "Titanic ML",
-        "Model_version": "0.2",
+        "Model_name": 'Titanic ML',
+        "Model_version": "0.3",
     }
 
 
 @app.get("/predict", tags=["Predict"])
 async def predict(
-    sex: str = "female", age: float = 29.0, fare: float = 16.5, embarked: str = "S"
+    sex: str = "female",
+    age: float = 29.0,
+    fare: float = 16.5,
+    embarked: str = "S"
 ) -> str:
-    """ """
+    """
+    """
 
     df = pd.DataFrame(
         {
@@ -43,6 +71,8 @@ async def predict(
     )
 
     prediction = int(model.predict(df)[0])
+
+    logging.debug(f"Predicting {prediction} for {df.to_dict()}")
 
     prediction = "Survived 🎉" if prediction == 1 else "Dead ⚰️"
 
